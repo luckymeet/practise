e4c66ed4827adfe22e70a4e42ebd0d4ccc7f3a56
yuminjun <yuminjun@lexiangbao.com>
2020-09-03 14:08:05 +0800

    代码提交

--- src/main/java/com/config/AppConfig.java
+++ src/main/java/com/config/AppConfig.java
@@ -1,9 +1,21 @@
 package com.config;
 
-import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
+
+@EnableAspectJAutoProxy
 @Configuration
-//@MapperScan("org.mybatis.spring.sample.mapper")
 public class AppConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource("jdbc:mysql://localhost:3306/test?serverTimezone=UTC", "root", "root.ycw");
+        driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        return driverManagerDataSource;
+    }
+
 }
